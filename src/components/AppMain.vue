6ad95b2b5879131fd69aaded850368330acfd606
Youss0203 <youssefhalibi003@gmail.com>
2024-02-20 22:48:53 +0100

    creato nuovo progetto e aggiunte card

---
+++ src/components/AppMain.vue
@@ -0,0 +1,64 @@
+<template lang="">
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card " v-for="project in projects" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">
+                {{ project.nome }}
+            </h5>
+               <p class="card-text">
+                {{project.descrizione}}
+            </p>
+             <p>
+                   {{project.linguaggi_usati}}
+             </p>
+             <p>
+                {{project.type.tipo}}
+             </p>
+    </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+
+    data() {
+        return {
+            projects:[],
+        }
+    },
+
+    methods:{
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                
+                }
+            })
+            .then((response)=> {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+            })
+            .catch(function (error) {
+                console.warn(error);
+            })
+            
+        }
+    },
+
+    created() {
+        this.getProjects();
+    },
+    
+}
+
+</script>
+<style lang="">
+    
+</style>
